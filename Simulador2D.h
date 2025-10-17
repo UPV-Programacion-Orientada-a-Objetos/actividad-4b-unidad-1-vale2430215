3aa59eefcdaea73366ab1735bab1249425d1d76d
Valeria Bermudez <2430215@upv.edu.mx>
2025-10-17 17:20:10 -0600

    Actividad 4 funciones en template

--- Simulador2D.h
+++ Simulador2D.h
@@ -1,3 +1,9 @@
+#ifndef SIMULADOR2D_H
+#define SIMULADOR2D_H
+
+#include <iostream>
+using namespace std;
+
 template <typename T>
 class Simulador2D {
 private:
@@ -22,4 +28,51 @@ public:
     void agregarFuente(T valor); // Expansión manual del vector dinámico si es necesario
     void simularPaso(); // Implementación del algoritmo
     // ... otros métodos de acceso y visualización
+    void mostrarGrid() const;
 };
+
+// Constructor
+template <typename T>
+Simulador2D<T>::Simulador2D(int f, int c) {
+    _filas = f;
+    _columnas = c;
+
+    // Crear matriz 2D
+    _grid = new T*[f];
+    for (int i = 0; i < f; i++) {
+        _grid[i] = new T[c];
+        for (int j = 0; j < c; j++)
+            _grid[i][j] = 0; // iniciar en 0
+    }
+
+    // Inicializar vector dinámico de fuentes
+    _capacidadFuentes = 2;
+    _numFuentes = 0;
+    _fuentes = new T[_capacidadFuentes];
+
+    // Inicializar constantes
+    _constantes[0] = 1.0f;
+    _constantes[1] = 1.0f;
+    _constantes[2] = 1.0f;
+}
+
+// Destructor
+template <typename T>
+Simulador2D<T>::~Simulador2D() {
+    for (int i = 0; i < _filas; i++)
+        delete[] _grid[i];
+    delete[] _grid;
+    delete[] _fuentes;
+}
+
+// Mostrar matriz
+template <typename T>
+void Simulador2D<T>::mostrarGrid() const {
+    for (int i = 0; i < _filas; i++) {
+        for (int j = 0; j < _columnas; j++)
+            cout << _grid[i][j] << "\t";
+        cout << endl;
+    }
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "Simulador2D.h"
+using namespace std;
 
 int main() {
+     Simulador2D<float> sim(5, 5);
+
+    
+    cout << "Simulacion completada" << endl;
     return 0;
 }
